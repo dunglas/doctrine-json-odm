2721cc2bb8e949df5258ff004d553fb6b6e362d4
Tamás Szigeti <tamas@szigeti.one>
2022-10-17 12:15:33 +0200

    lint

--- tests/Fixtures/TestBundle/CustomTypeMapper.php
+++ tests/Fixtures/TestBundle/CustomTypeMapper.php
@@ -1,6 +1,14 @@
 <?php
 
+/*
+ * (c) Kévin Dunglas <dunglas@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace Dunglas\DoctrineJsonOdm\Tests\Fixtures\TestBundle;
+
 use Dunglas\DoctrineJsonOdm\Tests\Fixtures\TestBundle\Document\WithMappedType;
 use Dunglas\DoctrineJsonOdm\TypeMapperInterface;
 
