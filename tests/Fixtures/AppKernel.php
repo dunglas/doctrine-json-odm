eec7e6cffd9b43677439d6b5a346868d2b6a3d81
Damien Carrier <damien.carrier@tiime.fr>
2025-12-02 16:20:19 +0100

    try fixing CI

--- tests/Fixtures/AppKernel.php
+++ tests/Fixtures/AppKernel.php
@@ -7,6 +7,7 @@
  * with this source code in the file LICENSE.
  */
 
+use Doctrine\Bundle\DoctrineBundle\Attribute\AsDoctrineListener;
 use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
 use Doctrine\DBAL\Types\JsonbType;
 use Doctrine\ORM\Proxy\Proxy;
@@ -52,26 +53,6 @@ class AppKernel extends Kernel
         ]);
 
         $orm = ['auto_mapping' => true];
-//        $ormMappings = [
-//            'TestBundle' => [
-//                'is_bundle' => false,
-//                'type' => 'attribute',
-//                'dir' => __DIR__.'/TestBundle/Entity',
-//                'prefix' => 'Dunglas\DoctrineJsonOdm\Tests\Fixtures\TestBundle\Entity',
-//                'alias' => 'TestBundle',
-//            ],
-//        ];
-//
-//        if (class_exists(JsonbType::class)) {
-//            $ormMappings['TestBundleJsonb'] = [
-//                'is_bundle' => false,
-//                'type' => 'attribute',
-//                'dir' => __DIR__.'/TestBundle/Entity/Jsonb',
-//                'prefix' => 'Dunglas\DoctrineJsonOdm\Tests\Fixtures\TestBundle\Entity\Jsonb',
-//                'alias' => 'TestBundleJsonb',
-//            ];
-//        }
-//        $orm = ['controller_resolver' => ['auto_mapping' => false], 'mappings' => $ormMappings];
 
         if (\PHP_VERSION_ID >= 80400 && !interface_exists(Proxy::class)) {
             $orm['enable_native_lazy_objects'] = true;
